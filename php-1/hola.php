ad8516a0635b9e82e2e7c0f7a7acaeb1d27dd7b4
Diego Mejia <mejiadiego17@17gmail.com>
2022-08-25 11:16:31 -0600

    primer ejercicio listo

---
+++ php-1/hola.php
@@ -0,0 +1,15 @@
+<?php
+$nombre  = $_POST['nombre'];
+$apellido  = $_POST['apellido'];
+$email  = $_POST['email'];
+$texto  = $_POST['texto'];
+$sexo  = $_POST['x'];
+
+echo "Nombre: ".$nombre."<br>";
+echo "Apellido: ".$apellido."<br>";
+echo "Texto: ".$texto."<br>";
+echo "Email: ".$email."<br>";
+echo "sexo: ".$sexo."<br> <td>";
+
+
+?>
